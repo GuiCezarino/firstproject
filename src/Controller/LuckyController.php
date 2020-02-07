2217258cffb59e333180aa9faff9a0f16572eee2
Guilherme Cezarino <guilhermesilva@medmkt.net>
2020-02-07 17:02:28 -0300

    Commits

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,15 +2,55 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-    public function number()
-  {
-      $number = random_int(0, 100);
 
+    /**
+     * @Route("/lucky/number", name="lucky_number")
+     */
+
+    public function getNumber()
+    {
+
+      $posts = [
+          [
+              'id' => 1,
+              'title' => 'Post 1',
+              'created_at' => '2020-02-07 14:20:01'
+          ],
+
+          [
+              'id' => 2,
+              'title' => 'Post 2',
+              'created_at' => '2020-02-08 14:20:01'
+          ],
+
+          [
+              'id' => 3,
+              'title' => 'Post 3',
+              'created_at' => '2020-02-09 14:20:01'
+          ]
+      ];
+
+
+      $getNumber = random_int(0, 100);
       return $this->render('lucky/number.html.twig', [
-          'number' => $number,
+          'title' => 'Este é um título teste',
+          'number' => $getNumber,
+          'posts' => $posts
       ]);
+  }
+
+  /**
+   * @Route("/post/{slug}")
+   */
+
+  public function single() {
+      return $this -> render('single.html.twig',
+      [
+          'slug' => $slug
+      ]
   }
 }
