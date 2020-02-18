0682029c2625579c2d3943a34bd03cfdb0112512
Guilherme Cezarino <guilhermesilva@medmkt.net>
2020-02-18 12:51:49 -0300

    Commits

---
+++ src/Controller/CetController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CetController extends AbstractController
+{
+
+    /**
+     * @Route("/cet/conteudo", name="cet_conteudo")
+     */
+
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+       return $this->render('lucky/number.html.twig', [
+           'number' => $number,
+       ]);
+    }
+}
